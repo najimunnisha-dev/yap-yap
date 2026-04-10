97d37cc01cf5881893eaafc5a4350ac5e2a7fba1
Najimunnisha A <najimunnishaa@gmail.com>
2026-04-10 20:38:23 +0530

    Added login page and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import LoginPage from "./pages/LoginPage";
 
 function App(){
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,45 @@
-function LoginPage(){
-    return(
-        <>
-        <h1>Yap-Yap</h1>
-        <p>Welcome</p>
-        <input type="email" placeholder="Email" required /> <br />
-        <input type="password" placeholder="Password" required /> <br />
-        <button type="submit">Login</button> <br />
-        <p>Don't have an account</p>
-        <a href="">Sign Up</a>
-        </>
+import { useState } from "react";
+import '../styles/LoginPage.css';
+
+function LoginPage() {
+
+    const [darkMode, setDarkMode] = useState(true);
+
+    const toggleTheme = () => {
+        setDarkMode(!darkMode);
+    };
+
+    return (
+        <div className={`main-container ${darkMode ? "dark" : "light"}`}>
+            
+            {/* Toggle Button */}
+            <div className="theme-toggle">
+                <button onClick={toggleTheme}>
+                    {darkMode ? "Dark" : "Light"}
+                </button>
+            </div>
+
+            <div className="left-panel">
+                <h1>Yap-Yap</h1>
+                <p>Yap it your way !!</p>
+            </div>
+
+            <div className="right-panel">
+                <div className="login-container">
+                    <h3>Welcome</h3>
+
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+
+                    <button>Login</button>
+
+                    <p>Don't have an account?</p>
+                    <a href="">Sign Up</a>
+                </div>
+            </div>
+
+        </div>
     )
 }
+
 export default LoginPage;
